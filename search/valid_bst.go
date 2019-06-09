7f06213189191b5c507cea1a265fe25b18767e8b
ligang <ligang1109@aliyun.com>
2019-06-09 20:23:56 +0800

    add valid_bst

---
+++ search/valid_bst.go
@@ -0,0 +1,29 @@
+package search
+
+import "math"
+
+type IntBTree struct {
+	Value int
+	Left  *IntBTree
+	Right *IntBTree
+}
+
+func ValidIntBst(btree *IntBTree) bool {
+	return validIntBst(btree, math.MinInt64, math.MaxInt64)
+}
+
+func validIntBst(btree *IntBTree, min, max int) bool {
+	if btree == nil {
+		return true
+	}
+
+	if btree.Value <= min || btree.Value >= max {
+		return false
+	}
+
+	if !validIntBst(btree.Left, min, btree.Value) {
+		return false
+	}
+
+	return validIntBst(btree.Right, btree.Value, max)
+}
